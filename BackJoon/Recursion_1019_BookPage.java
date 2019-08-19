4f6dd36a8f8f7ca496a6ff8e815e17ea8b8d9d38
IlsanJerry <superpcw1080@naver.com>
2019-08-19 23:49:17 +0900

    Recursion

---
+++ BackJoon/Recursion_1019_BookPage.java
@@ -0,0 +1,50 @@
+package BackJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Recursion_1019_BookPage {
+  
+    private static int[] arr = new int[10];
+    private static int location = 1;
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine()); // 36
+        int start = 1;
+        int end = n;
+        
+        while(start <= end) {    
+            while(start%10!=0 && start<=end){
+                cal(start);
+                start++; 
+            }
+           
+            if(start > end) break;             
+            while(end%10!=9 && start<=end){
+                cal(end);
+                end--; 
+            }         
+            start /= 10;  
+            end /= 10;  
+            
+            for(int i=0 ; i<10 ; i++) arr[i] += (end-start+1) * location;        
+            location *= 10;  
+        }
+        
+        StringBuilder builder = new StringBuilder();
+        for(int i=0 ; i<10 ; i++) builder.append(arr[i] + " ");
+        
+        System.out.println(builder.toString());
+    }
+    
+    public static void cal(int num){ // 각 숫자가 몇 번씩 나오는지 더함
+        while(num > 0){
+        	arr[num%10] += location;
+            num /= 10;
+        }
+    }
+}
+
