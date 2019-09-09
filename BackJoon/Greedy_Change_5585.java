b29edbd2b98ef2421b6260e22872befa58b88d72
IlsanJerry <superpcw1080@naver.com>
2019-09-09 23:57:29 +0900

    DP

--- BackJoon/Greedy_Change_5585.java
+++ BackJoon/Greedy_Change_5585.java
@@ -11,6 +11,7 @@ public class Greedy_Change_5585 {
     		new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String args[]) throws IOException {
+    	
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int money = 1000 - (Integer.parseInt(br.readLine()));
         
